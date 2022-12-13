2356d05335109da4c7bb80a417d1571f53c909b2
Abdelrahman_Rashad <a.rashaad98@gmail.com>
2022-12-13 17:03:18 +0200

    add card-list and search-field components

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class CardList extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                HELLO THERE FORM CARD-LIST
+            </div>
+        );
+    }
+}
+
+export default CardList;
